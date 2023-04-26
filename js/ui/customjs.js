d916ada36e874032ef2191ed1a7adcecefab1d6b
win0509 <dbtmdfl109@naver.com>
2023-04-26 12:48:27 +0900

    md art 탭기능준비

--- js/ui/customjs.js
+++ js/ui/customjs.js
@@ -1,5 +1,30 @@
+
+
+//------------ HEADER HIDE AND SHOW ------------------
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+let prevScrollpos = window.pageYOffset;
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', function(){
+  const currentScrollPos = window.pageYOffset;
+  if(currentScrollPos > 150){
+    if(prevScrollpos > currentScrollPos){
+      //헤더 나타남
+      header.style.top = 0
+    }else{
+      //헤더 사라짐
+      header.style.top = -100 + '%';
+    }
+    prevScrollpos = currentScrollPos; //마우스 이동 후 스크롤 위치값 재할당
+  }else{
+
+  }
+});
+
+
+
 //------------ BEST ITEMS SLID------------------
-const swiper = new Swiper('.best-image-wrapper .swiper', {
+const bestArtSwiper = new Swiper('.best-image-wrapper .swiper', {
 slidesPerView : 4,
 spaceBetween: 15,
 
